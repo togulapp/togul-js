fc9962daeb8950cf5eddf2ca491d1391003cc6ce
Onur Kaçmaz <kacmaz.onur@hotmail.com>
2026-03-29 02:31:28 +0300

    fix: enable code splitting to share TogulContext across entry points

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   },
   format: ["cjs", "esm"],
   dts: true,
-  splitting: false,
+  splitting: true,
   sourcemap: true,
   clean: true,
   external: ["react", "next"],
